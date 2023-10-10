4622b61dd3adceebc2b26949a380978045d8758d
Veronika <veronika.vorontsova1@gmail.com>
2023-10-10 20:57:12 +0300

    edit println

--- src/New.java
+++ src/New.java
@@ -1,9 +1,9 @@
-Class New {
+public class New {
     public static void main(String args[]) {
       int a = 10;
       a +=1;
       
-      System.out.println(a);
+      System.out.println("Hello World "+ a);
       
     }
 }
